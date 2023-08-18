b13050e51b581209b4ea2fcbdb53e98f7f0796e7
taks <857tn859@gmail.com>
2023-08-18 09:11:45 +0900

    Add Secure Server test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include <NimBLEDevice.h>
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  NimBLEDevice::init("NimBLE");
+
+  NimBLEDevice::setSecurityAuth(true, true, true);
+  NimBLEDevice::setSecurityPasskey(123456);
+  NimBLEDevice::setSecurityIOCap(BLE_HS_IO_DISPLAY_ONLY);
+
+  NimBLEServer *pServer = NimBLEDevice::createServer();
+  NimBLEService *pService = pServer->createService("ABCD");
+  NimBLECharacteristic *pSecureCharacteristic =
+    pService->createCharacteristic(
+      "1235",
+      NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::READ_ENC | NIMBLE_PROPERTY::READ_AUTHEN |
+      NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::WRITE_ENC | NIMBLE_PROPERTY::WRITE_AUTHEN );
+
+  pService->start();
+  pSecureCharacteristic->setValue("Hello Secure BLE");
+
+  NimBLEAdvertising *pAdvertising = NimBLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID("ABCD");
+  pAdvertising->start();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
